c036b9a53999396fb1d854eb8166f6c80e34cf7b
Thierry Guedj <thguedj@gmail.com>
2020-09-20 20:10:17 +0200

    Updated designs-listing

--- layouts/designs-listing.vue
+++ layouts/designs-listing.vue
@@ -266,6 +266,7 @@ export default {
   mounted() {
     this.$nextTick(function () {
       this.hideModal()
+      this.$auth.fetchUser()
       window.addEventListener('scroll', function () {
         const navbar = document.getElementById('nav')
         const navClasses = navbar.classList
