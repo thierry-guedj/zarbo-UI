fa03542a9bc210ea105eb5fa938c7b7dea0c02ec
Thierry Guedj <thguedj@gmail.com>
2020-09-28 14:38:26 +0200

    Updated PrivacyPolicy

--- pages/PrivacyPolicy.vue
+++ pages/PrivacyPolicy.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script>
-export default {}
+import RGPD from '@components/RGPD.vue'
+export default {
+  layouts: 'designs-listing',
+  components: {
+    RGPD,
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
