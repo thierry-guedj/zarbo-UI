f42d24b173c39789bd16c93180943ad4321b3d73
Thierry Guedj <thguedj@gmail.com>
2020-09-28 02:38:26 +0200

    Updated Create

--- pages/user/designs/Create.vue
+++ pages/user/designs/Create.vue
@@ -321,7 +321,7 @@ export default {
           }, 3000)
           const imageUrl =
             'https://les2hiboux.s3.eu-west-3.amazonaws.com/uploads/designs/minithumbnail/' +
-            response.data.image
+            response.data.images.thumbnail
           fetch(imageUrl, { method: 'HEAD' })
             .then((res) => {
               if (res.ok) {
