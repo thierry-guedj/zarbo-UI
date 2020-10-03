1c814d85a09f7d899f72b1d4fc723324b13b47c5
Thierry Guedj <thguedj@gmail.com>
2020-10-03 11:57:48 +0200

    Some css on User page

--- pages/designs/User.vue
+++ pages/designs/User.vue
@@ -1,191 +1,198 @@
 <template>
   <section>
     <div v-if="$fetchState.pending" class="loader">
-    <Circle8></Circle8>
-  </div>
-  <div v-else class="pt-6 pl-6">
-    
-    <v-btn
-      v-show="fab"
-      v-scroll="onScroll"
-      fab
-      dark
-      fixed
-      bottom
-      right
-      color="accent"
-      @click="toTop"
-    >
-      <v-icon>keyboard_arrow_up</v-icon>
-    </v-btn>
+      <Circle8></Circle8>
+    </div>
+    <div v-else class="pt-6 pl-6">
+      <v-btn
+        v-show="fab"
+        v-scroll="onScroll"
+        fab
+        dark
+        fixed
+        bottom
+        right
+        color="accent"
+        @click="toTop"
+      >
+        <v-icon>keyboard_arrow_up</v-icon>
+      </v-btn>
 
-    <v-container class="p-0 m-0 row-designs">
-      <section class="hero text-center text-white">
-        <v-container>
-          <v-row class="row-md-12">
-            <v-col class="col-md-3 avatarUser">
-              <avatar
-                :username="user.name"
-                :src="user.avatars.large"
-                class="ml-3 mr-2"
-                :size="240"
-              ></avatar>
-            </v-col>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-col class="col-md-9">
-              <p class="titleUser text-white">
-                {{ $t('user.userPageTitle') }} {{ user.name }}
-              </p>
-              <p class="tagline text-white">
-                {{ user.tagline }}
-              </p>
-              <p class="about text-white">
-                {{ user.about }}
-              </p>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-container fluid class="search-control">
-          <form @submit.prevent="fetchData">
-            <v-row
-              class="filters d-flex justify-content-between align-items-center"
-            >
-              <v-col cols="auto"
-                ><v-select
-                  v-model="filters.orderBy"
-                  :items="itemsOrderBy"
-                  item-text="title"
-                  item-value="value"
-                  :label="$t('search.orderBy')"
-                  outlined
-                  width="250px"
-                  @change="fetchData"
-                ></v-select
-              ></v-col>
-
-              <v-col cols="auto" class="d-flex align-items-center">
-                <v-checkbox
-                  id="has_comments"
-                  v-model="filters.has_comments"
-                  field="has_comments"
-                  :label="$t('search.hasComments')"
-                  class="mr-3"
-                  true-value="1"
-                  false-value="0"
-                  @change="fetchData"
-                ></v-checkbox>
+      <v-container class="p-0 m-0 row-designs">
+        <section class="hero text-center text-white">
+          <v-container>
+            <v-row class="row-md-12">
+              <v-col class="col-md-3 avatarUser">
+                <avatar
+                  :username="user.name"
+                  :src="user.avatars.large"
+                  class="ml-3 mr-2"
+                  :size="240"
+                ></avatar>
               </v-col>
-              <v-col>
-                <v-text-field
-                  id="q"
-                  v-model="filters.q"
-                  field="q"
-                  class="combobox"
-                  outlined
-                  placeholder="rechercher dans les titres et les descriptions"
-                  @input="fetchData"
-                >
-                  <template v-slot:append>
-                    <v-btn
-                      :disabled="searching"
-                      class="searchBtn"
-                      height="auto"
-                      text
-                      :loading="searching"
-                      type="submit"
-                    >
-                      <v-icon>mdi-magnify</v-icon>
-                      {{ $t('search.search') }}
-                    </v-btn>
-                  </template>
-                </v-text-field>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-col class="col-md-9">
+                <p class="titleUser text-white">
+                  {{ $t('user.userPageTitle') }} {{ user.name }}
+                </p>
+                <p class="tagline text-white">
+                  {{ user.tagline }}
+                </p>
+                <p class="about text-white">
+                  {{ user.about }}
+                </p>
               </v-col>
             </v-row>
-          </form>
-        </v-container>
-      </section>
-      <div v-if="searching" class="loader p-0">
-        <Circle8></Circle8>
-      </div>
-      <div v-else class="pl-0 pb-6 pr-0">
-        <template v-if="(!designs.length)" class="pb-6 text-center">
-          <div class="mx-auto">
-            <v-alert
-              border="right"
-              color="accent"
-              dark
-              width="60%"
-              class="alert"
-            >
-              <!-- On teste pourquoi il n'y a pas de résultat -->
-              <!-- 1° cas : les critères de recherche ne donnent aucun résultat -->
-              <!-- 2° cas : l'artiste n'a pas publié d'oeuvre -->
-              {{ $t('user.noResult') }}
-              <v-spacer />
-              <nuxt-link :to="{ name: 'users.search' }">
-                <v-btn class="mt-3"
-                  ><v-icon right dark class="mx-2">reply</v-icon
-                  >{{ $t('user.backToUsersList') }}</v-btn
-                >
-              </nuxt-link>
-            </v-alert>
-          </div>
-        </template>
-        <template v-else id="row-designs">
-          <!-- <nuxt-link :to="{ name: 'users.search' }">
+          </v-container>
+          <v-container fluid class="search-control">
+            <form @submit.prevent="fetchData">
+              <v-row
+                class="filters d-flex justify-content-between align-items-center"
+              >
+                <v-col cols="auto"
+                  ><v-select
+                    v-model="filters.orderBy"
+                    :items="itemsOrderBy"
+                    item-text="title"
+                    item-value="value"
+                    :label="$t('search.orderBy')"
+                    outlined
+                    width="250px"
+                    @change="fetchData"
+                  ></v-select
+                ></v-col>
+
+                <v-col cols="auto" class="d-flex align-items-center">
+                  <v-checkbox
+                    id="has_comments"
+                    v-model="filters.has_comments"
+                    field="has_comments"
+                    :label="$t('search.hasComments')"
+                    class="mr-3"
+                    true-value="1"
+                    false-value="0"
+                    @change="fetchData"
+                  ></v-checkbox>
+                </v-col>
+                <v-col>
+                  <v-text-field
+                    id="q"
+                    v-model="filters.q"
+                    field="q"
+                    class="combobox"
+                    outlined
+                    placeholder="rechercher dans les titres et les descriptions"
+                    @input="fetchData"
+                  >
+                    <template v-slot:append>
+                      <v-btn
+                        :disabled="searching"
+                        class="searchBtn"
+                        height="auto"
+                        text
+                        :loading="searching"
+                        type="submit"
+                      >
+                        <v-icon>mdi-magnify</v-icon>
+                        {{ $t('search.search') }}
+                      </v-btn>
+                    </template>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </form>
+          </v-container>
+        </section>
+        <div v-if="searching" class="loader p-0">
+          <Circle8></Circle8>
+        </div>
+        <div v-else class="pl-0 pb-6 pr-0">
+          <template v-if="(!designs.length)" class="pb-6 text-center">
+            <div class="mx-auto">
+              <v-alert
+                border="right"
+                color="accent"
+                dark
+                width="60%"
+                class="alert"
+              >
+                <!-- On teste pourquoi il n'y a pas de résultat -->
+                <!-- 1° cas : les critères de recherche ne donnent aucun résultat -->
+                <!-- 2° cas : l'artiste n'a pas publié d'oeuvre -->
+                {{ $t('user.noResult') }}
+                <v-spacer />
+                <nuxt-link :to="{ name: 'users.search' }">
+                  <v-btn class="mt-3"
+                    ><v-icon right dark class="mx-2">reply</v-icon
+                    >{{ $t('user.backToUsersList') }}</v-btn
+                  >
+                </nuxt-link>
+              </v-alert>
+            </div>
+          </template>
+          <template v-else id="row-designs">
+            <!-- <nuxt-link :to="{ name: 'users.search' }">
             <v-btn class="mt-3"
               ><v-icon right dark class="mx-2">reply</v-icon
               >{{ $t('user.backToUsersList') }}</v-btn
             >
           </nuxt-link> -->
-          <v-row
-            transition-duration="0.3s"
-            item-selector=".item"
-            class="mb-6 row-design"
-            justify="center"
-            no-gutters
-          >
-            <CoolLightBox
-              :items="itemsDesigns"
-              :index="index !== null ? parseInt(`${index}`) : index"
-              :use-zoom-bar="true"
-              :effect="'fade'"
-              @close="index = null"
+            <v-row
+              transition-duration="0.3s"
+              item-selector=".item"
+              class="mb-6 row-design"
+              justify="center"
+              no-gutters
             >
-            </CoolLightBox>
-            <masonry
-              :cols="{ default: 6, 1500: 5, 1400: 4, 1000: 3, 700: 2, 500: 1 }"
-              :gutter="{ default: '0px', 700: '15px' }"
-              ><lazy-component
-                v-for="(design, i) in designs"
-                :key="`${i}-${design.id}`"
-                :design="design"
-                @lightbox="index = parseInt(`${i}`)"
-              ></lazy-component
-            ></masonry>
-          </v-row>
+              <CoolLightBox
+                :items="itemsDesigns"
+                :index="index !== null ? parseInt(`${index}`) : index"
+                :use-zoom-bar="true"
+                :effect="'fade'"
+                @close="index = null"
+              >
+              </CoolLightBox>
+              <masonry
+                :cols="{
+                  default: 6,
+                  1500: 5,
+                  1400: 4,
+                  1000: 3,
+                  700: 2,
+                  500: 1,
+                }"
+                :gutter="{ default: '0px', 700: '15px' }"
+                ><lazy-component
+                  v-for="(design, i) in designs"
+                  :key="`${i}-${design.id}`"
+                  :design="design"
+                  @lightbox="index = parseInt(`${i}`)"
+                ></lazy-component
+              ></masonry>
+            </v-row>
 
-          <infinite-loading
-            ref="infiniteLoading"
-            slot="append"
-            :identifier="identifier"
-            spinner="ring-loader"
-            force-use-infinite-wrapper="row-designs"
-            @infinite="infiniteHandler"
-          >
-          </infinite-loading>
-        </template>
-      </div>
-    </v-container>
-    <!-- Modal  -->
-    <!-- <keep-alive> -->
-    <base-modal
-      :dialog.sync="visible"
-      @showDesign="styleModal()"
-      @closeDialog="hideModal()"
-    />
-    <!-- </keep-alive> -->
-    <!-- End Modal -->
+            <infinite-loading
+              ref="infiniteLoading"
+              slot="append"
+              :identifier="identifier"
+              spinner="ring-loader"
+              force-use-infinite-wrapper="row-designs"
+              @infinite="infiniteHandler"
+            >
+            </infinite-loading>
+          </template>
+        </div>
+      </v-container>
+      <!-- Modal  -->
+      <!-- <keep-alive> -->
+      <base-modal
+        :dialog.sync="visible"
+        @showDesign="styleModal()"
+        @closeDialog="hideModal()"
+      />
+      <!-- </keep-alive> -->
+      <!-- End Modal -->
+    </div>
   </section>
 </template>
 
@@ -328,6 +335,7 @@ export default {
   font-size: 44px;
   padding-top: 20px;
   margin-bottom: 30px;
+  line-height: 1em;
 }
 .tagline {
   font-size: 24px;
@@ -361,7 +369,9 @@ export default {
   top: 50%;
   left: 50%;
 }
-
+.col-md-2 {
+  padding: 0;
+}
 .image {
   height: 300px;
   width: 300px;
