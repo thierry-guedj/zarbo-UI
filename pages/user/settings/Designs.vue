482cddeda15ca80aecc1363f8167192a7b55d240
Thierry Guedj <thguedj@gmail.com>
2020-10-06 11:10:11 +0200

    Updated Designs

--- pages/user/settings/Designs.vue
+++ pages/user/settings/Designs.vue
@@ -166,7 +166,7 @@ export default {
   data() {
     return {
       form: this.$vform({
-        title: this.editItem.title,
+        title: '',
         description: '',
         is_live: '',
         tags: [],
@@ -312,7 +312,7 @@ export default {
       this.editedIndex = this.designs.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.editedItem.tags = this.editedItem.tag_list.tags
-      // this.$v.form = this.editItem
+      this.form = this.editItem
       this.dialog = true
     },
     confirmDeleteItem(item) {
