eec464b29c6592418ab9c97db631456a04caed26
Thierry Guedj <thguedj@gmail.com>
2020-10-09 21:49:44 +0200

    Updated Designs

--- pages/user/settings/Designs.vue
+++ pages/user/settings/Designs.vue
@@ -440,7 +440,7 @@ export default {
           description: this.$v.form.description.$model,
           is_live: this.editedItem.is_live,
           tags: this.simpleStringArrayTags,
-          tag: this.editedItem.tag,
+          // tag: this.editedItem.tag,
           assign_to_team: false,
           team: null,
           tag_list: {
