4a7a899a45335a0ab70e260e2f5abbb3e89e4d7b
Thierry Guedj <thguedj@gmail.com>
2020-10-23 15:17:24 +0200

    Added loading to asyncdata

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'server',
+
+  // custom loading
+  loading: '~/components/Loading.vue',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
