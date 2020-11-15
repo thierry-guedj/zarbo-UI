b91de04dc6a07af003513627f5faf881c82369ec
Thierry Guedj <thguedj@gmail.com>
2020-11-15 15:39:12 +0100

    Lazy test

--- pages/designs/Search.vue
+++ pages/designs/Search.vue
@@ -139,12 +139,12 @@
                   500: 1,
                 }"
                 :gutter="{ default: '0px', 700: '15px' }"
-                ><lazy-component
+                ><DesignCard
                   v-for="(design, i) in designs"
                   :key="`${i}-${design.id}`"
                   :design="design"
                   @lightbox="index = parseInt(`${i}`)"
-                ></lazy-component
+                ></DesignCard
               ></masonry>
             </v-row>
 
@@ -179,7 +179,7 @@ export default {
   name: 'Search',
   layout: 'designs-listing',
   components: {
-    lazyComponent: () => import('@/components/designs/DesignCard.vue'),
+    // lazyComponent: () => import('@/components/designs/DesignCard.vue'),
     Circle8,
     CoolLightBox,
   },
